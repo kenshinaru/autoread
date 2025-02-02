e0ad79026159b6e2396693d9117a0c38573572e8
kenshinaru <121244764+kenshinaru@users.noreply.github.com>
2025-02-02 21:56:41 +0700

    Update setting.js

--- plugins/setting.js
+++ plugins/setting.js
@@ -5,13 +5,24 @@ export default {
    command: ['anticall', 'readsw', 'reactsw', 'readchat', 'autotyping', 'self', 'online'],
    tags: 'setting',
    run: async (m, { sock, text, command, setting }) => {
-      const option = text.toLowerCase()
-      if (!['on', 'off'].includes(option)) return sock.reply(m.from, `🚩 Enter on/off`, m)
-      setting[command] = option === 'on'
-      const path = './setting.js'
-      const newContent = `export default ${JSON.stringify(setting, null, 3)}\n`
-      fs.writeFileSync(path, newContent, 'utf-8')
-      sock.reply(m.from, `🚩 ${command} has been ${option === 'on' ? 'activated' : 'inactivated'} successfully.`, m)
+      if (!text) {
+         let allStatus = Object.keys(setting)
+            .filter(key => typeof setting[key] === 'boolean')
+            .map(key => `- ${key} = ${setting[key] ? 'ON' : 'OFF'}`)
+            .join('\n');
+         return sock.reply(m.from, `Masukkan ON/OFF\n\nCurrent all status:\n${allStatus}`, m);
+      }
+
+      if (!['on', 'off'].includes(text.toLowerCase())) {
+         return sock.reply(m.from, `Masukkan ON/OFF`, m);
+      }
+
+      setting[command] = text.toLowerCase() === 'on';
+      const path = './setting.js';
+      const newContent = `export default ${JSON.stringify(setting, null, 3)}\n`;
+      fs.writeFileSync(path, newContent, 'utf-8');
+
+      sock.reply(m.from, `Berhasil ${text.toLowerCase() === 'on' ? 'Mengaktifkan' : 'Menonaktifkan'} ${command}`, m);
    },
    owner: true
-}
+};
