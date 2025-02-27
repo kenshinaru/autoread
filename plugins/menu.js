87d4cb78d6daa0f6fb89de999a44360c38e3b3a8
kenshinaru <121244764+kenshinaru@users.noreply.github.com>
2025-02-27 11:14:57 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,47 +1,34 @@
 export default {
-   name: 'menu',
-   command: ['menu'],
-   tags: 'main',
+   name: "menu",
+   command: ["menu"],
+   tags: "main",
    run: async (m, { sock, plugins }) => {
-    try {
-      const categories = Object.values(plugins).reduce((acc, menu) => {
-         if (!menu.tags) return acc;
-         const cmds = Array.isArray(menu.name) ? menu.name : [menu.name];
-         acc[menu.tags] = acc[menu.tags] || new Set();
-         cmds.forEach(cmd => acc[menu.tags].add(cmd));
-         return acc;
-      }, {});
+      try {
+         let cat = {};
+         Object.values(plugins).forEach(p => p.tags && (cat[p.tags] ??= new Set()).add(...[].concat(p.name)));
 
-      let message = "Halo! Berikut adalah daftar menu yang tersedia:\n\n" +
-         Object.entries(categories)
-            .sort()
-            .map(([category, cmds]) =>
-               `🔹 \`${category.toUpperCase()}\`\n` + 
-               [...cmds].sort().map((cmd, i, arr) => 
-                  ` ${i === arr.length - 1 ? '└' : '├'} ${cmd}`
-               ).join('\n')
-            ).join('\n\n') + '\n\n> https://github.com/kenshinaru/autoread';
-
-      
-      return m.reply({
-         text: message.trim(), 
-         contextInfo: {
-            mentionedJid: sock.parseMentions(message),
-            isForwarded: true,
-            externalAdReply: {
-               title: `Hi, ${m.pushName}`,
-               thumbnailUrl: 'https://files.catbox.moe/26b7a0.png',
-               sourceUrl: 'https://github.com/kenshinaru/autoread',
-               mediaType: 1,
-               previewType: 0,
-               renderLargerThumbnail: true,
+         m.reply({
+            text:
+               "Halo! Berikut daftar menu:\n\n" +
+               Object.entries(cat)
+                  .map(([c, cmds]) => `🔹 \`${c.toUpperCase()}\`\n${[...cmds].map((cmd, i, a) => ` ${i == a.length - 1 ? "└" : "├"} ${cmd}`).join``}`)
+                  .join`\n\n` +
+               "\n\n> https://github.com/kenshinaru/autoread",
+            contextInfo: {
+               mentionedJid: [],
+               externalAdReply: {
+                  title: `Hi, ${m.pushName}`,
+                  thumbnailUrl: "https://files.catbox.moe/26b7a0.png",
+                  sourceUrl: "https://github.com/kenshinaru/autoread",
+                  mediaType: 1,
+                  previewType: 0,
+                  renderLargerThumbnail: true,
+               },
             },
-         },
-      })
+         });
       } catch (e) {
-        console.log(e)
-        m.reply(e)
-        }
+         console.log(e), m.reply(e);
+      }
    },
-   location: __filename
+   location: __filename,
 };
