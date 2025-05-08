51e6b62d867f264718927a3490afc0c6ebfb0d1c
kenshinaru <121244764+kenshinaru@users.noreply.github.com>
2025-05-08 11:40:09 +0700

    Delete plugins/test.js

--- plugins/test.js
+++
@@ -1,8 +0,0 @@
-export default {
-	name: "Testing",
-	command: ["test", "testing"],
-	models: "%prefix%command",
-	run: async(m, { sock }) => {
-		await m.reply(`Halo ${m.pushName} aya naon? :D`);
-	}
-}
